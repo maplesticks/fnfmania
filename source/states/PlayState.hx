74820330c3ab278269c4eb53e9349e2fb9521222
cutzye <102763844+cutzye@users.noreply.github.com>
2023-07-24 22:41:15 -0400

    changes...

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -45,7 +45,7 @@ import states.editors.CharacterEditorState;
 
 import substates.PauseSubState;
 import substates.GameOverSubstate;
-import substates.GameOverSubstate;
+import substates.ResultsSubState;
 
 #if !flash 
 import flixel.addons.display.FlxRuntimeShader;
@@ -80,16 +80,15 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['F', 0.2], //From 0% to 19%
+		['E-', 0.4], //From 20% to 39%
+		['D-', 0.5], //From 40% to 49%
+		['C-', 0.6], //From 50% to 59%
+		['C+', 0.69], //From 60% to 68%
+		['B-', 0.8], //From 70% to 79%
+		['B+', 0.9], //From 80% to 89%
+		['AA', 1], //From 90% to 99%
+		['AAAA', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 
 	//event variables
@@ -2360,6 +2359,10 @@ class PlayState extends MusicBeatState
 					cancelMusicFadeTween();
 					LoadingState.loadAndSwitchState(new PlayState());
 				}
+			}
+			else if(ClientPrefs.data.ratingScreenPopUp)
+			{
+		//		openSubstate(new ResultsSubState(boyfriend.getScreenPosition().x, boyfriend.getScreenPosition().y)); making this substate into a function in playstate
 			}
 			else
 			{
